58040652ff50668e57cf59fed82559f793eac691
Leticia Aponte Bahena <letiaponte@gmail.com>
2019-06-29 02:42:03 -0500

    Document format to EmplooyeeRepositoryH2

--- src/main/java/com/kenzan/employeeservice/repositories/EmployeeRepositoryH2.java
+++ src/main/java/com/kenzan/employeeservice/repositories/EmployeeRepositoryH2.java
@@ -3,5 +3,5 @@ package com.kenzan.employeeservice.repositories;
 import com.kenzan.employeeservice.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepositoryH2 extends JpaRepository<Employee,Long> {
+public interface EmployeeRepositoryH2 extends JpaRepository<Employee, Long> {
 }
