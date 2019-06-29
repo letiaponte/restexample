becaf7b02fe70815dffbd1fa5a496527ab23289d
Leticia Aponte Bahena <letiaponte@gmail.com>
2019-06-29 02:18:45 -0500

    Create employee model

---
+++ src/main/java/com/kenzan/employeeservice/models/Employee.java
@@ -0,0 +1,34 @@
+package com.kenzan.employeeservice.models;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@Entity
+public class Employee {
+    private @Id
+    @GeneratedValue
+    Long id;
+    private String firstName;
+    private String middleInitial;
+    private String lastName;
+    private Date dateOfBirth;
+    private Date dateOfEmployment;
+    private boolean status;
+
+    Employee() {
+    }
+
+    public Employee(String firstName, String middleInitial, String lastName, Date dateOfBirth, Date dateOfEmployment, boolean status) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfEmployment = dateOfEmployment;
+        this.status = status;
+    }
+}
