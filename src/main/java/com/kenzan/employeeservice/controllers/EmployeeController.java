9789f2065d0725ba1b483d1948b53fb63e133c79
Leticia Aponte Bahena <letiaponte@gmail.com>
2019-07-08 02:35:07 -0500

    Refactoring of EmployeeController and RootController to add index URI

--- src/main/java/com/kenzan/employeeservice/controllers/EmployeeController.java
+++ src/main/java/com/kenzan/employeeservice/controllers/EmployeeController.java
@@ -2,11 +2,9 @@ package com.kenzan.employeeservice.controllers;
 
 import com.kenzan.employeeservice.exceptions.EmployeeNotFoundException;
 import com.kenzan.employeeservice.models.Employee;
-import com.kenzan.employeeservice.repositories.EmployeeRepository;
 import com.kenzan.employeeservice.services.EmployeeServiceImpl;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
-import org.springframework.hateoas.core.DummyInvocationUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,47 +12,47 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping(value = "/employees")
 public class EmployeeController {
 
     private final EmployeeServiceImpl employeeService;
-    private final RootController assembler;
+    private final RootController rootController;
 
     EmployeeController(EmployeeServiceImpl employeeService,
                        RootController assembler) {
 
         this.employeeService = employeeService;
-        this.assembler = assembler;
+        this.rootController = assembler;
     }
 
-    @GetMapping("/employees")
+    @GetMapping("/")
     public Resources<Resource<Employee>> findAll() {
         List<Resource<Employee>> employees = new ArrayList<>();
         Iterable <Employee> all = employeeService.findAll();
         for (Employee employee:all){
-            employees.add(assembler.toResource(employee));
+            employees.add(rootController.toResource(employee));
         }
 
         return new Resources<>(employees,
                 linkTo(methodOn(EmployeeController.class).findAll()).withSelfRel());
     }
 
-    @GetMapping("/employees/{id}")
+    @GetMapping("/{id}")
     public Resource<Employee> findById(@PathVariable Long id) {
 
         Employee employee = employeeService.findById(id);
 
-        return assembler.toResource(employee);
+        return rootController.toResource(employee);
     }
 
-    @PostMapping("/employees")
+    @PostMapping("/")
     ResponseEntity<?> save(@RequestBody Employee newEmployee) throws URISyntaxException {
-        Resource<Employee> resource = assembler.toResource(employeeService.save(newEmployee));
+        Resource<Employee> resource = rootController.toResource(employeeService.save(newEmployee));
 
         return ResponseEntity
                 .created(new URI(resource.getId().expand().getHref()))
@@ -63,19 +61,19 @@ public class EmployeeController {
 
 
 
-    @PutMapping("/employees/{id}")
+    @PutMapping("/{id}")
     ResponseEntity<?> update (@RequestBody Employee newEmployee, @PathVariable Long id) throws URISyntaxException {
 
         Employee updatedEmployee = employeeService.update(newEmployee, id);
 
-        Resource<Employee> resource = assembler.toResource(updatedEmployee);
+        Resource<Employee> resource = rootController.toResource(updatedEmployee);
 
         return ResponseEntity
                 .created(new URI(resource.getId().expand().getHref()))
                 .body(resource);
     }
 
-    @DeleteMapping("/employees/{id}")
+    @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
 
         boolean response = employeeService.delete(id);
--- src/main/java/com/kenzan/employeeservice/controllers/RootController.java
+++ src/main/java/com/kenzan/employeeservice/controllers/RootController.java
@@ -1,15 +1,29 @@
 package com.kenzan.employeeservice.controllers;
 
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 import com.kenzan.employeeservice.models.Employee;
 
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
+@RequestMapping(value = "/")
 public class RootController implements ResourceAssembler<Employee, Resource <Employee>> {
+
+    @GetMapping("/")
+    ResponseEntity<ResourceSupport> index() {
+        ResourceSupport resourceSupport = new ResourceSupport();
+        resourceSupport.add(linkTo(methodOn(RootController.class).index()).withSelfRel());
+        resourceSupport.add(linkTo(methodOn(EmployeeController.class).findAll()).withRel("employees"));
+        return ResponseEntity.ok(resourceSupport);
+    }
 
     @Override
     public Resource<Employee> toResource (Employee employee) {
